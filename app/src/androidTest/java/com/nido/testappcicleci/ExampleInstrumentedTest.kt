965e479262b23301c75ad7aa64b2234aa14fe6de
naveedahmad <sr.naveed99@gmail.com>
2021-10-04 18:03:03 +0500

    Lint and other changes added

--- app/src/androidTest/java/com/nido/testappcicleci/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/nido/testappcicleci/ExampleInstrumentedTest.kt
@@ -1,11 +1,10 @@
+@file:Suppress("SpellCheckingInspection")
 package com.nido.testappcicleci
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
 import org.junit.Test
 import org.junit.runner.RunWith
-
 import org.junit.Assert.*
 
 /**
